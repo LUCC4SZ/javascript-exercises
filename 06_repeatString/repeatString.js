f236833b08c15ab1881e63bcbbcec6dd366d642d
lucca-dev <luccazamora@gmail.com>
2025-10-21 18:35:33 -0300

    Finish exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, max) {
+    if (max < 0) return "ERROR";
+    let repeatedString = "";
+    for (let i = 1; i <= max; i++) {
+        repeatedString = repeatedString.concat(str);
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
