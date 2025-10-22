763ac8c3337ddef04d029482e3c25f6f12e8909d
lucca-dev <luccazamora@gmail.com>
2025-10-22 12:13:56 -0300

    Finish exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let rvstr = "";
+    const arrStr = str.split("");
+    for (let i = arrStr.length - 1; i >= 0; i--) {
+        rvstr += arrStr[i];
+    }
+    return rvstr;
 };
 
 // Do not edit below this line
