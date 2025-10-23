e6ad4b10e185f14e394fcc36413c390335772c0f
lucca-dev <luccazamora@gmail.com>
2025-10-23 15:59:29 -0300

    Finish exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
+    let total = 0;
+    if (a > b){
+        for (let i = a; i >= b; i--) {
+            total += i;
+        }
+    } else {
+        for (let i = a; i <= b; i++) {
+            total += i;
+        }
+    }
+    return total;
 };
 
 // Do not edit below this line
