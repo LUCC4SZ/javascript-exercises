50e2aef6439e66258e00baf6f92e84e114437fea
lucca-dev <luccazamora@gmail.com>
2025-10-23 14:16:57 -0300

    Pass seventh test

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,11 +1,9 @@
-const removeFromArray = function(arr, a, b, c, d) {
-    const arrTwo = [a, b, c, d];
-    for (let i = 0; i <= arr.length; i++) {
-        if (arr[i] === arrTwo[i]) {
-            arr = arr.slice(i, 0);
-        }
+const removeFromArray = function(arr, a, b) {
+    if (arr.includes(a) && arr.includes(b)) {
+        const indexA = arr.indexOf(a);
+        const indexB = arr.indexOf(b);
+        return arr.slice(0, indexA).concat(arr.slice(indexA + 1, indexB)).concat(arr.slice(indexB + 1));
     }
-    return arr;
 };
 
 // Do not edit below this line
