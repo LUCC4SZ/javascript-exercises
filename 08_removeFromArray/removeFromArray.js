e6e7e5fc7d1c2bb44e2843515766cd606d9d9b72
lucca-dev <luccazamora@gmail.com>
2025-10-23 15:21:50 -0300

    Finish exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,13 +1,12 @@
-const removeFromArray = function(arr, a, b) {
-    if (arr.includes(a)) {
-        const indexA = arr.indexOf(a);
-        arr = arr.slice(0, indexA).concat(arr.slice(indexA + 1));
-    }
-    if (arr.includes(b)) {
-        const indexB = arr.indexOf(b);
-        arr = arr.slice(0, indexB).concat(arr.slice(indexB + 1));
-    }
-    return arr;
+const removeFromArray = function(arr, ...args) {
+    /* TOP solution */
+    const newArr = [];
+    arr.forEach(element => {
+        if (!args.includes(element)) {
+            newArr.push(element);
+        }
+    });
+    return newArr;
 };
 
 // Do not edit below this line
