d7730ec1b37e475f5f7bb3bf8f5dcf7e1c89fa6a
lucca-dev <luccazamora@gmail.com>
2025-10-23 16:31:16 -0300

    Finish exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  const converted = (x - 32) * (5 / 9);
+  const rounded = Math.round(converted * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  const converted = x * (9 / 5) + 32;
+  const rounded = Math.round(converted * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
