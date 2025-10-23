e947cac0b818a0a75c1a621f24b4af2c21284a14
lucca-dev <luccazamora@gmail.com>
2025-10-23 13:24:17 -0300

    Pass first test

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, item) {
+    if (arr.includes(item)) {
+        const index = arr.indexOf(item);
+        return arr.slice(0, index).concat(arr.slice(index + 1));
+    }
+
 };
 
 // Do not edit below this line
