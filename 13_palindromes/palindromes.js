2395ee18921c085c2f2af5ad899e98f4a9f686b4
lucca-dev <luccazamora@gmail.com>
2025-12-20 23:15:43 -0300

    Finish exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,33 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    const punctuation = ["!", ",", ".", "?"];
+
+    for (let i = 0; i < word.length; i++) {
+        for (let j = 0; j <= punctuation.length; j++) {
+            if (word[i] === punctuation[j]) {
+                word = word.slice(0, i).concat(word.slice(i + 1,));
+            }
+        }
+    }
+
+    word = word.toLowerCase();
+
+    word = word.replaceAll(" ", "");
+
+    let pal = "";
+
+    const arr = [];
+
+    for (let i = 0; i < word.length; i++) {
+        arr[i] = word[i];
+    };
+
+    const reversed = arr.toReversed();
+    for (let i = 0; i < reversed.length; i++) {
+        pal+= reversed[i];
+    };
+
+    return word === pal;
 };
 
 // Do not edit below this line
