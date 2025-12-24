48a51419a70717abdb8a13ac33dfdca90bc549af
lucca-dev <luccazamora@gmail.com>
2025-12-24 16:14:35 -0300

    Finish exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    let fib = 0;
+    let ant1 = 0;
+    let ant2 = 1;
+    if (pos < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 1; i <= pos; i++) {
+            fib = ant1 + ant2;
+            ant2 = ant1;
+            ant1 = fib;
+        }
+    }
+    return fib;
 };
 
 // Do not edit below this line
