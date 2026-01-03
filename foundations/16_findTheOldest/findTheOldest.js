40d05485da9c0423b7a5e2b5b02c05ca45ec22bc
lucca-dev <luccazamora@gmail.com>
2026-01-03 15:31:44 -0300

    Pass first test

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let ages = array.map((item) => {
+        return {name: item.name, age: item.yearOfDeath - item.yearOfBirth};
+    });
 
+    let oldest;
+    for (let i = 0; i <= ages.length; i++) {
+        initial = ages[0].age;
+        oldest = ages.filter((item) => item.age > initial);
+        inital = ages[i];
+    }
+    return oldest[0];
 };
 
 // Do not edit below this line
